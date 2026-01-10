df238dec1dcd4046465df42840786924c36ae532
VoidNoVerges <241015669+VoidNoVerges@users.noreply.github.com>
2026-01-10 17:34:44 +0100

    cards animated on hover & resizable

--- src/client.rs
+++ src/client.rs
@@ -11,7 +11,7 @@ use iced::{
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use tokio_tungstenite::{connect_async, tungstenite::Message as WsMessage};
-use indexmap::IndexMap;
+use indexmap::{IndexMap, map::MutableKeys};
 
 use crate::api::{ServerMessage, C};
 
@@ -22,6 +22,9 @@ static CARD1_PATH:&'static str = "assets/cards/1.png";
 static CARD2_PATH:&'static str = "assets/cards/2.png";
 static CARD3_PATH:&'static str = "assets/cards/3.png";
 static CARD4_PATH:&'static str = "assets/cards/4.png";
+static MULT_BASE_WIDTH_CARD_WIDTH: f32 = 0.12;
+static MULT_BASE_WIDTH_CARD_HEIGHT: f32 = MULT_BASE_WIDTH_CARD_WIDTH * 1.46;
+static MULT_BASE_WIDTH_CARD_STACK_OFFSET: f32 = MULT_BASE_WIDTH_CARD_HEIGHT * 0.53;
 
 #[derive(Debug)]
 struct App {
@@ -33,13 +36,14 @@ struct App {
 
     window_size: Size,
     cards: IndexMap<usize, Card>,
+    card_base_size: Size,
     focus_card_row_low: bool,
     top_card_id_upper: usize,
-    top_card_id_lower: usize
-
+    top_card_id_lower: usize 
 }
 
 impl Default for App {
+
     fn default() -> Self {
         Self {
             connected: false,
@@ -69,10 +73,95 @@ impl Default for App {
                 (16, Card::new(CARD3_PATH, Size::new(154.0, 225.0))),
                 (17, Card::new(CARD2_PATH, Size::new(154.0, 225.0))),
             ]),
+            card_base_size: Size::new(154.0, 225.0),
             focus_card_row_low: true,
             top_card_id_upper: 100, // Impossible to reach
             top_card_id_lower: 100  // Impossible to reach
         }
+    }
+}
+
+impl App {
+
+    fn get_card(&self, id: usize) -> &Card {
+        self.cards.get(&id).unwrap()
+    }
+
+    fn get_card_mut(&mut self, id: usize) -> &mut Card {
+        self.cards.get_mut(&id).unwrap()
+    }
+
+    fn get_card_ids(&self) -> Vec<usize> {
+        let mut card_ids: Vec<usize> = vec!();
+        for card in self.cards.iter() {
+            card_ids.push(*card.0);
+        }
+        card_ids
+    }
+
+    fn get_hand_width(&self) -> f32 {
+        // 10 cards layered size with max width reached while left and right card at max size
+        self.card_base_size.width * 3.0 +
+        self.card_base_size.width * 1.1
+    }
+
+    fn get_hand_height(&self) -> f32 {
+        // This does ignore size multiplication of cards sinze their dimensions move down
+        // while the card is moving up so the total length of the card hand is not altered by that.
+        self.card_base_size.height -  // card bottom
+        self.get_hand_row_distance() +  // card top
+        self.card_base_size.height * 0.15  // card offset
+    }
+
+    fn get_hand_width_offset(&self) -> f32 {
+        (self.card_base_size.width * 1.1 - self.card_base_size.width) / 2.0
+    }
+
+    fn get_hand_height_offset(&self) -> f32 {
+        self.get_hand_height() - self.card_base_size.height
+    }
+
+    fn get_hand_row_distance(&self) -> f32 {
+        -self.window_size.width * MULT_BASE_WIDTH_CARD_STACK_OFFSET
+    }
+
+    fn get_card_width(&self) -> f32 {
+        self.window_size.width * MULT_BASE_WIDTH_CARD_WIDTH
+    }
+
+    fn get_card_height(&self) -> f32 {
+        self.window_size.width * MULT_BASE_WIDTH_CARD_HEIGHT
+    }
+
+    fn get_upper_row_card_spawn_point(&self) -> Point {
+
+        let max_row_len: f32 = 4.0 * self.card_base_size.width;
+
+        let row_y_offset: f32 = self.get_hand_row_distance();
+
+        let mut row_x_offset: f32 = 0.0;
+        if self.cards.len() > 10 {
+            let cards_in_row: usize = self.cards.len() - 10;
+            let row_len: f32 = (cards_in_row as f32) * self.card_base_size.width / 3.0 +
+                                     self.card_base_size.width * (2.0/3.0);
+            row_x_offset = (max_row_len - row_len) / 2.0;
+        }
+
+        Point::new(row_x_offset, row_y_offset)
+    }
+
+    fn get_lower_row_card_spawn_point(&self) -> Point {
+
+        let max_row_len: f32 = 4.0 * self.card_base_size.width;
+
+        let cards_in_row: usize = std::cmp::min(self.cards.len(), 10);
+        let row_len: f32 = (cards_in_row as f32) * self.card_base_size.width / 3.0 +
+                           self.card_base_size.width * (2.0/3.0);
+        let row_x_offset: f32 = (max_row_len - row_len) / 2.0;
+        let row_y_offset: f32 = 0.0;
+
+        Point::new(row_x_offset, row_y_offset)
+
     }
 }
 
@@ -86,18 +175,62 @@ enum AppMessage {
     WindowResized(Size),
     CardPlayed(usize),
     CardHovered(usize),
+    CardNotHovered(usize),
+    FrameTick,
 }
 
 #[derive(Debug)]
 struct Card {
     img_path: &'static str,
+    offset: f32,
     size: Size,
+    size_mult: f32,
+    moving_up: CardMoveState,
 }
 
 impl Card {
+
     fn new(img_path: &'static str, size: Size) -> Self{
-        Card {img_path: img_path, size: size}
+        Card {
+            img_path: img_path,
+            size: size,
+            size_mult: 1.0,
+            offset: 0.0,
+            moving_up: CardMoveState::NotMoving
+        }
     }
+
+    fn move_card_up(&mut self) {
+        let max_card_offset: f32 = self.size.height * 0.15;
+        if self.moving_up == CardMoveState::MovingUp && self.offset <= max_card_offset {
+            self.size_mult += 0.02;
+            self.offset += max_card_offset * 0.2;
+        }
+        else if self.moving_up != CardMoveState::MovingDown {
+            self.moving_up = CardMoveState::NotMoving;
+        }
+    }
+
+    fn move_card_down(&mut self) {
+        let max_card_offset: f32 = self.size.height * 0.15;
+        if self.moving_up == CardMoveState::MovingDown && self.offset > 0.0 {
+            self.size_mult -= 0.02;
+            self.offset -= max_card_offset * 0.20;
+        }
+        else if self.moving_up != CardMoveState::MovingUp {
+            self.moving_up = CardMoveState::NotMoving;
+            // Correcting floating point error
+            self.size_mult = 1.0;
+            self.offset = 0.0;
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum CardMoveState {
+    MovingUp,
+    MovingDown,
+    NotMoving
 }
 
 fn update(state: &mut App, msg: AppMessage) -> Task<AppMessage> {
@@ -147,9 +280,9 @@ fn update(state: &mut App, msg: AppMessage) -> Task<AppMessage> {
             Task::none()
         }
         AppMessage::CardHovered(card_id) => {
-            if state.cards.len() > 10 && state.cards.iter()
-                                                    .take(state.cards.len() - 10)
-                                                    .any(|(key, _)| *key == card_id) {
+            state.get_card_mut(card_id).moving_up = CardMoveState::MovingUp;
+            if state.cards.len() > 10 && state.get_card_ids()[..state.cards.len() - 10]
+                                                             .contains(&card_id) {
                     state.focus_card_row_low = false;
                     state.top_card_id_upper = card_id;
                 } else {
@@ -162,8 +295,33 @@ fn update(state: &mut App, msg: AppMessage) -> Task<AppMessage> {
             println!("Card with id {} played!", card_id);
             Task::none()
         }
+        AppMessage::CardNotHovered(card_id) => {
+            state.get_card_mut(card_id).moving_up = CardMoveState::MovingDown;
+            Task::none()
+        }
+        AppMessage::FrameTick => {
+            for card_id in state.get_card_ids().iter() {
+                if state.get_card(*card_id).moving_up == CardMoveState::MovingUp {
+                    state.get_card_mut(*card_id).move_card_up();
+                }
+                if state.get_card(*card_id).moving_up == CardMoveState::MovingDown {
+                    state.get_card_mut(*card_id).move_card_down();
+                }
+                if *card_id != state.top_card_id_lower &&
+                   *card_id != state.top_card_id_upper &&
+                   state.get_card_mut(*card_id).offset != 0.0 {
+                        state.get_card_mut(*card_id).move_card_down();
+                }
+            };
+            Task::none()
+        }
         AppMessage::WindowResized(size) => {
             state.window_size = size;
+            state.card_base_size = Size::new(state.get_card_width(),
+                                             state.get_card_height());
+            for (_, card) in state.cards.iter_mut2() {
+                card.size = state.card_base_size;
+            };
             Task::none()
         } // Send C messages if needed
     }
@@ -219,15 +377,18 @@ async fn connect_ws(ws_tx: WsConnection, server_rx: ServerMsgReceiver, ip: Strin
     }
 }
 
-fn view_card<'a>(card_id: &usize, card: &Card, x_pos: f32) -> Pin<'a, AppMessage> {
+fn view_card<'a>(card_id: &usize, card: &Card, x_pos: f32, y_pos: f32) -> Pin<'a, AppMessage> {
         pin(
             (MouseArea::new(image(card.img_path)
-                     .width(card.size.width).height(card.size.height)))
+                     .width(card.size.width * card.size_mult)
+                     .height(card.size.height * card.size_mult)))
                      .on_double_click(AppMessage::CardPlayed(*card_id))
                      .on_enter(AppMessage::CardHovered(*card_id))
+                     .on_exit(AppMessage::CardNotHovered(*card_id))
                      .interaction(Interaction::Pointer)
         )
-        .position(Point{x: x_pos, y: 0.0})
+        .position(Point::new(x_pos + (card.size.width - card.size.width * card.size_mult) / 2.0,
+                             y_pos-(card.offset as f32)))
     }
 
 fn view_hand<'a>(state: &App) -> Pin<'a, AppMessage> {
@@ -237,8 +398,11 @@ fn view_hand<'a>(state: &App) -> Pin<'a, AppMessage> {
     let mut card_stack_upper: Stack<'_, AppMessage> = stack!();
     let mut card_stack_lower: Stack<'_, AppMessage> = stack!();
 
-    // Push all cards in state.cards to their row with a x-offset.
-    let mut x_pos = 0.0;
+    // Push all cards in state.cards to their row.
+    let mut x_pos: f32 = 0.0;
+    let y_pos: f32 = 0.0;
+    let x_pos_offset: f32 = state.get_hand_width_offset();
+    let y_pos_offset: f32 = state.get_hand_height_offset();
     let mut move_card_stack_lower = true;
     if state.cards.len() > 10 {
         move_card_stack_lower = false;
@@ -247,7 +411,8 @@ fn view_hand<'a>(state: &App) -> Pin<'a, AppMessage> {
 
     for (i, (card_id, card)) in state.cards.iter().enumerate() {
 
-        let viewable_card: Pin<'_, AppMessage> = view_card(card_id, card, x_pos);
+        let viewable_card: Pin<'_, AppMessage> = view_card(card_id, card,
+            x_pos + x_pos_offset, y_pos + y_pos_offset);
 
         if move_card_stack_lower {
             if push_lower {
@@ -266,7 +431,7 @@ fn view_hand<'a>(state: &App) -> Pin<'a, AppMessage> {
         if (!move_card_stack_lower && *card_id == state.top_card_id_upper) ||  // Top card reached
            (move_card_stack_lower && *card_id == state.top_card_id_lower) {
                 push_lower = true;
-            }
+        }
 
         x_pos = x_pos + card.size.width / 3.0;
 
@@ -277,26 +442,21 @@ fn view_hand<'a>(state: &App) -> Pin<'a, AppMessage> {
         }
     }
 
-    // Calculate the y-offset of the upper/lower card row
-    let card_stack_upper_offset = Point::new(0.0, 0.0);
-    let card_stack_lower_offset = Point::new(0.0, 100.0);
-
     // Add the upper/lower row to the whole hand.
     if state.focus_card_row_low {
             card_stack = card_stack.push(pin(card_stack_upper)
-                                                    .position(card_stack_upper_offset));
+                                            .position(state.get_upper_row_card_spawn_point()));
             card_stack = card_stack.push(pin(card_stack_lower)
-                                                    .position(card_stack_lower_offset));
+                                            .position(state.get_lower_row_card_spawn_point()));
         } else {
             card_stack = card_stack.push(pin(card_stack_lower)
-                                                    .position(card_stack_lower_offset));
+                                            .position(state.get_lower_row_card_spawn_point()));
             card_stack = card_stack.push(pin(card_stack_upper)
-                                                    .position(card_stack_upper_offset));
+                                            .position(state.get_upper_row_card_spawn_point()));
         }
 
-    pin(card_stack)
+    pin(card_stack).width(state.get_hand_width()).height(state.get_hand_height())
 }
-
 
 fn view(state: &'_ App) -> Element<'_, AppMessage> {
     let gui_switch: usize = 0;
@@ -322,6 +482,8 @@ fn subscription(state: &App) -> Subscription<AppMessage> {
     let mut subscriptions: Vec<Subscription<AppMessage>> = vec!();
     subscriptions.push(window::resize_events()
                        .map(|(_, size)| AppMessage::WindowResized(size)));
+    subscriptions.push(time::every(Duration::from_millis(16))
+                       .map(|_| AppMessage::FrameTick));
     if state.connected {
         subscriptions.push(time::every(Duration::from_millis(100)).map(|_| AppMessage::Tick));
     };
