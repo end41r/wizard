81579f0bac89bb2cbc03e59767d94641d058e30f
Andrei Kurochkin <andi.kurochkin@gmail.com>
2026-02-20 16:28:12 +0100

    Flip the windows console logic :)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Enable only in release before the final build.
 // Disables the console window on Windows.
-#![cfg_attr(feature = "wiz_debug", windows_subsystem = "windows")]
+#![cfg_attr(not(feature = "wiz_debug"), windows_subsystem = "windows")]
 
 mod animation;
 mod api;
